b573e11491abcfab16b503441b8ecca4c286f4bb
franoo <franciszekkrawczyk@protonmail.com>
2022-02-15 21:08:08 +0100

    testing get http method

---
+++ WebApi/WebApi/Helpers/DataGenerator.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApi.Context;
+using WebApi.Models;
+
+namespace WebApi.Helpers
+{
+    public class DataGenerator
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new DatabaseContext(
+                serviceProvider.GetRequiredService<DbContextOptions<DatabaseContext>>()))
+            {
+                if (context.Albums.Any())
+                {
+                    return;
+                }
+                context.Albums.AddRange(
+                    new Album
+                    {
+                        Id = 1,
+                        Title = "Kompilacja",
+                        ArtistName = "Koniec Lata",
+                        Version = "epka",
+                        ReleaseDate = DateTime.Now.AddDays(-1),
+                        DistributorId = 1,
+                    });
+                context.SaveChanges();
+            }
+        }
+    }
+}
