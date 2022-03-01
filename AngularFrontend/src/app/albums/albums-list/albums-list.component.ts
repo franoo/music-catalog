bcd1ef850d392e73e09314075343ca2fbed0109d
franoo <franciszekkrawczyk@protonmail.com>
2022-03-01 21:16:18 +0100

    getting list of logged user from angular with token authorization

--- AngularFrontend/src/app/albums/albums-list/albums-list.component.ts
+++ AngularFrontend/src/app/albums/albums-list/albums-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Album } from 'src/app/models/album.model';
+import { AlbumService } from 'src/app/services/album.service';
 
 @Component({
   selector: 'app-albums-list',
@@ -7,9 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AlbumsListComponent implements OnInit {
 
-  constructor() { }
+  constructor(private albumService: AlbumService) { }
+
+  albums: Album[]=[];
+  selectedAlbum?: Album;
 
   ngOnInit(): void {
+    this.albumService.getAlbums().subscribe((data:Album[])=>{
+      this.albums = data;
+      console.log(this.albums);
+    })
   }
-
 }
